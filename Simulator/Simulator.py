edef286cc7f5ea2c469b7ae6fdcc080e50667cfe
Eduardo Vannini <eduardo.vannini@student.uclouvain.be>
2020-04-30 15:40:07 +0200

    Version finale de 'Simulator.py'

--- Simulator/Simulator.py
+++ Simulator/Simulator.py
@@ -23,7 +23,7 @@ class Simulator(abc.ABC):
     d/dt x(t) = f(x, u, vu)
     y(t)      = h(x, u, yu)
 
-    La valeur des differentes variables du systeme sont enregistrees pour un maximum de 'buffer_size' periodes.
+    La valeur des differentes variables du systeme sont enregistrees pour un maximum de 'buffer_size' iterations.
     Cette classe n'est pas faite pour etre instanciee. Il faut en faire de l'heritage et implementer les
     methodes non implementees en fonction du modele que l'on veut simuler.
     """
@@ -60,7 +60,7 @@ class Simulator(abc.ABC):
         :param n_states    : Nombre d'etats du systeme.
         :param n_commands  : Nombre de commandes du systeme.
         :param n_outputs   : Nombre de sorties du systeme.
-        :param dt          : Periode d'echantillonnage pour la discretisation du systeme.
+        :param dt          : Periode d'echantillonnage pour la discretisation du systeme [s].
         :param buffer_size : Nombre maximal de donnees enregistrables par le systeme.
         """
 
@@ -91,7 +91,7 @@ class Simulator(abc.ABC):
     def dxdt(self):
         """
         Implementation de la fonction 'f(x, u, vu)' pour calculer la derivee de l'etat au temps 'timestep' * 'dt'.
-        Il ne faut pas inclure de bruit dans cette fonction car on est dans lapartie "theorique" du systeme, alors
+        Il ne faut pas inclure de bruit dans cette fonction car on est dans la partie "theorique" du systeme, alors
         que le bruit n'intervient que lors de la partie "pratique": les interactions avec le systeme.
 
         Note: Lors de l'appel de cette fonction, le vecteur 'all_x' aura ete rempli jusqu'a l'indice
@@ -99,6 +99,7 @@ class Simulator(abc.ABC):
 
         :return: Vecteur contenant la derivee de l'etat au temps 'timestep'
         """
+        # En l'occurence on est dans la classe abstraite. Les implementations se feront dans les sous-classes.
         raise NotImplementedError("Method 'dxdt' in class 'Simulator' must be overridden.")
 
     @abc.abstractmethod
@@ -109,6 +110,7 @@ class Simulator(abc.ABC):
 
         :return: Vecteur contenant la sortie non-bruitee au temps 'timestep' * 'dt' (sortie theorique).
         """
+        # En l'occurence on est dans la classe abstraite. Les implementations se feront dans les sous-classes.
         raise NotImplementedError("Method 'y' in class 'Simulator' must be overridden.")
 
     def dudt(self):
@@ -118,7 +120,7 @@ class Simulator(abc.ABC):
 
         Voir: https://en.wikipedia.org/wiki/Finite_difference_coefficient#Backward_finite_difference
 
-        :return: Vecteur contenant une approximation dela derivee de la commande au temps 'timestep' * 'dt'.
+        :return: Vecteur contenant une approximation de la derivee de la commande au temps 'timestep' * 'dt'.
         """
         if self.timestep == 0:
             # Approximation triviale (manque de donnees)
@@ -163,7 +165,7 @@ class Simulator(abc.ABC):
                                     output_noise_func(timestep, params, all_t, all_u, all_y, dt)
         :param n_steps            : Nombre de pas de temps desires. Le nombre effectif de pas de temps est donne par:
                                     min(n_steps, buffer_size)
-        :param init_state         : Etat initial du systeme sous forme d'un array comportant n_states elements.
+        :param init_state         : Etat initial du systeme sous forme d'un array comportant 'n_states' elements.
         :return                   : None
         """
         # Initialisation du systeme
@@ -178,7 +180,7 @@ class Simulator(abc.ABC):
 
         # Boucle principale de la simulation
         while self.timestep < min(self.buffer_size, n_steps):
-            # 1) Calcul de la commande au temps 'timestep' sur base des pas du systeme au temps 'timestep'
+            # 1) Calcul de la commande au temps 'timestep' sur base de l'etat du systeme au temps 'timestep'
             u = command_func(self.timestep, self.params, self.all_t, self.all_u, self.all_y, self.dt)
             u_noise = command_noise_func(self.timestep, self.params, self.all_t, self.all_u, self.all_y, self.dt)
             self.all_u[self.timestep] = u + u_noise
@@ -196,14 +198,19 @@ class Simulator(abc.ABC):
 
             self.timestep += 1
 
+        # Gestion du cas ou le buffer est rempli avant la fin de la simulation
         if self.timestep == self.buffer_size and self.buffer_size <= n_steps < np.inf:
             raise RuntimeWarning("Simulation stopped as the simulation buffer is full.")
 
 
+# La suite du code ne sera executee que si le fichier 'Simulator.py' est lance directement. Elle ne le sera pas si ce
+# fichier est utilise comme import dans un autre fichier. La section ci-dessous sert de code de demonstration pour
+# l'utilisation des classes implementees ci-dessus.
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
 
     # Exemple avec le systeme y = x et dx/dt = -x
+    # Pour modeliser ce systeme, on herite de la classe 'Simulator' et on implemente les methodes abstraites
     class ExpSimulator(Simulator):
         def __init__(self, dt=0.05, buffer_size=10000):
             n_states, n_commands, n_outputs = 1, 0, 1
@@ -215,21 +222,34 @@ if __name__ == "__main__":
         def y(self):
             return self.all_x[self.timestep]
 
-
+    # Implementation de fonctions de bruit sur la commande et sur la sortie. En l'occurence, on ne met pas de bruit sur
+    # la commande mais on met un bruit sur la sortie.
     def my_command_func(timestep, params, all_t, all_u, all_y, dt):
         return np.zeros((0,))
 
     def my_output_noise(timestep, params, all_t, all_u, all_y, dt):
-        return (np.random.random(1) * 2 - 1) * 0.025  # Erreur de 2.5cm sur la position lors de la mesure
+        # Erreur uniformement repartie entre -2.5cm et 2.5cm sur la position lors de la mesure
+        return (np.random.random(1) * 2 - 1) * 0.025
 
 
+    # Pour pouvoir estre utilise, le simulateur doit etre instancie. On specifie 'buffer_size=1000' et 'dt=0.01',
+    # c'est-a-dire que les simulations auront une duree maximale de 10s (1000 * 0.01 = 10)
     sim = ExpSimulator(dt=0.01, buffer_size=1000)
+
+    # On definit egalement l'etat initil du systeme. Ici, on choisit x = 0m
     my_init_state = np.array([1])
 
+    # On lance la simulation avec la methode 'simulate' de notre simulateur, en specifiant les bruits et l'etat initial
     sim.simulate(my_command_func, output_noise_func=my_output_noise, init_state=my_init_state)
-    plt.plot(sim.all_t, sim.all_y, "k-", linewidth=1, label="Measured position")
-    plt.plot(sim.all_t, sim.all_x, "b-", linewidth=2, label="Actual position")
+
+    # On termine en recuperant les donnees de la simulation afin de les afficher dans un graphe
+    # Dans ce graphe, on affiche la position simulee non-bruitee ('Actual position'), la position mesuree en incluant
+    # le bruit ('Measured position') ainsi que la solution analytique du modele mathematique ('f(t) = exp(-t)').
+    plt.plot(sim.all_t, sim.all_y, "k-", linewidth=1, label="Measured position [m]")
+    plt.plot(sim.all_t, sim.all_x, "b-", linewidth=2, label="Actual position [m]")
     plt.plot(sim.all_t, np.exp(-sim.all_t), "r--", linewidth=1, label="f(t) = exp(-t)")
+    plt.xlabel("Time [s]")
+    plt.ylabel("Position [m]")
     plt.grid()
     plt.legend()
     plt.show()
